593c371d9f7448345fb981dcf847e65326602c03
Nicolas Louvet <nicolas.louvet@univ-lyon1.fr>
2024-07-23 16:27:34 +0200

    working in progress

---
+++ ascii2k7/ascii2k7.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3 
+
+def prep_code(infile):
+	code = b''
+	for line in infile:
+		line = line.upper().strip()
+		print(line)
+		code += (line.encode('ascii') + b'\x0d')
+	return code
+
+def checksum(code):
+	c = 0
+	for w in code:
+		c = (c + w) % 256
+	return c
+
+def synchro():
+	return b'\x01' * 16 + b'\x3c\x5a'
+
+def heading_bloc(name, ext):
+	bloc = b''
+	bloc += b'\x00'     # bloc type is "heading"
+	bloc += b'\x10'     # bloc length is 16 = \x10
+	data = b''
+	data += '{:8s}'.format(name).encode('ascii') # file name
+	data += '{:3s}'.format(ext).encode('ascii')  #file extension
+	data += b'\x00'     # file type: 00=Basic 01=Data 02=Binaire
+	data += b'\xff'     # file mode: 00=Binaire FF=Texte
+	data += b'\xff'     # same byte as the previous one
+	bloc += data
+	bloc += (256 - checksum(data)).to_bytes(1, 'little')
+	return bloc
+
+def ending_bloc():
+	code = b''
+	# bloc type is "ending"
+	code += b'\xff'
+	# bloc length is 2 = \x02
+	code += b'\x02'
+	# checksum is 0
+	code += b'\x00'   
+	return code 
+
+
+def file_bloc(data):
+	bloc = b''
+	# bloc type is "file content"
+	bloc += b'\x01'
+	# bloc length
+	bloc += ((len(data) + 2) % 256).to_bytes(1, 'little')
+	# bloc content
+	bloc += data
+	# checksum
+	bloc += (256 - checksum(data)).to_bytes(1, 'little')
+	return bloc
+
+def file_blocs(data):
+	code = b''
+	l = len(data)
+	i = 0
+	j = 254
+	while j < l:
+		tmp = data[i:j]
+		code += synchro()
+		code += file_bloc(tmp)
+		i += 254
+		j += 254
+	if i < l:
+		tmp = data[i:l]
+		code += synchro()
+		code += file_bloc(tmp)
+	return code
+
+def cvt(infile, name, ext):
+	outcode = b''
+	outcode += synchro()
+	outcode += heading_bloc(name, ext)
+	outcode += file_blocs(prep_code(infile))
+	outcode += synchro()
+	outcode += ending_bloc()
+	return outcode
+
+if __name__ == '__main__':
+	try:
+		infile = open('test.txt', 'r')
+		outcode = cvt(infile, 'TEST', 'BAS')
+		for i in range(len(outcode)):
+			print(hex(outcode[i]))
+		infile.close()
+		outfile = open('test.k7', 'wb')
+		outfile.write(outcode)
+		outfile.close()
+	except KeyboardInterrupt:
+		print()
+		print('Exiting the test...')
+		ser.close()
+
