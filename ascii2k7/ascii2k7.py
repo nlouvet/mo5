8273610c97eb5062735ec841f3017ff68036172e
Nicolas Louvet <nicolas.louvet@univ-lyon1.fr>
2024-07-24 22:47:40 +0200

    maj

--- ascii2k7/ascii2k7.py
+++ ascii2k7/ascii2k7.py
@@ -2,12 +2,110 @@
 
 import argparse
 
+class Converter:
+
+	infile = None
+	data = b''
+
+	def prep_basic(self):
+		self.data = b''
+		for line in infile:
+			line = line.strip()
+			self.data += (line.encode('ascii') + b'\x0d')
+
+	def __init__(self, infile_):
+		infile = infile_
+		self.prep_basic()
+
+	def checksum(sefl, chunck):
+		c = 0
+		for w in chunck:
+			c = (c + w) % 256
+		return c
+
+	def gen_synchro(self):
+		return b'\x01' * 16 + b'\x3c\x5a'
+	
+	def gen_head_bloc(self, name, ext):
+		bloc = self.gen_synchro()
+		# bloc type is "heading" = \x00
+		bloc += b'\x00'
+		# bloc length is 16 = \x10
+		bloc += b'\x10'
+		content = b''
+		# file name
+		content += '{:8s}'.format(name).encode('ascii')
+		#file extension
+		content += '{:3s}'.format(ext).encode('ascii')
+		# file type: 00=Basic 01=Data 02=Binaire
+		content += b'\x00'
+		# file mode: 00=Binaire FF=Texte
+		content += b'\xff'
+		# same byte as the previous one
+		content += b'\xff'
+		bloc += content
+		# checksum
+		bloc += (256 - checksum(content)).to_bytes(1, 'little')
+		return bloc
+	
+	def gen_end_bloc(self):
+		code = self.gen_synchro()
+		# bloc type is "ending"
+		code += b'\xff'
+		# bloc length is 2 = \x02
+		code += b'\x02'
+		# checksum is 0
+		code += b'\x00'   
+		return code 
+
+	def gen_bloc(self, data):
+		bloc = self.gen_synchro()
+		# bloc type is "file content"
+		bloc += b'\x01'
+		# bloc length
+		bloc += ((len(data) + 2) % 256).to_bytes(1, 'little')
+		# bloc content
+		bloc += data
+		# checksum
+		bloc += (256 - checksum(data)).to_bytes(1, 'little')
+		return bloc
+
+	def gen_blocs(self):
+		blocs = []
+		l = len(self.data)
+		i = 0
+		j = 254
+		while j < l:
+			tmp = self.gen_bloc(self.data[i:j])
+			blocs.append(self.gen_bloc(self.data[i:j]))
+			i += 254
+			j += 254
+		if i < l:
+			blocs.append(self.gen_bloc(self.data[i:l]))
+		return blocs
+	
+	def cvt(self, name, ext):
+		outcode = b''
+		outcode += self.gen_head_bloc(name, ext)
+		outcode += self.gen_blocs()
+		outcode += self.gen_end_bloc()
+		return outcode
+
+
+
+
+
+
+	
+
+
+
 def prep_code(infile):
 	code = b''
 	for line in infile:
 		#line = line.upper().strip()
 		line = line.strip()
-		print(line)
+		#print(line)
 		code += (line.encode('ascii') + b'\x0d')
 	return code
 
@@ -98,9 +196,17 @@ if __name__ == '__main__':
 		print(err)
 		exit(1)
 	
-	outcode = cvt(infile, 'TEST', 'BAS')
-	for i in range(len(outcode)):
-		print(hex(outcode[i]))
+	#outcode = cvt(infile, 'TEST', 'BAS')
+	outcode = b''
+	#infile.seek(0)
+	conv = Converter(infile)
+	blocs = conv.gen_blocs()
+	for i in range(len(blocs)):
+		print(f"Bloc #{i:d}")
+		print(''.join(f'{w:02x} ' for w in blocs[i]))
+
+	#for i in range(len(outcode)):
+	#	print(hex(outcode[i]))
 	outfile.write(outcode)
 
 	infile.close()
